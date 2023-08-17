5c48f5a7f891fb0e75e81381971ef7a51041c408
dizbaru-99 <142541939+dizbaru-99@users.noreply.github.com>
2023-08-17 21:56:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-r8qcw7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-r8qcw7)
---
+++ index.ts
@@ -0,0 +1,30 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let mg: number;
+let mh: number;
+mg = 200;
+mh = 300;
+console.log('mh');
+console.log('kkk');
+console.log(mh + mg);
+let aa: number;
+let bb: number;
+aa = 10;
+bb = 30;
+let cc: number;
+cc = aa + bb;
+console.log(cc);
+let dd: number;
+let ee: number;
+dd = 20;
+ee = aa + bb + cc + dd;
+console.log(ee);
+let mmhg: number;
+mmhg = 20;
+mmhg = mmhg + 2220;
+console.log(mmhg);
+let boba: string;
+boba = 'وووووووووواو';
+console.log(boba);
